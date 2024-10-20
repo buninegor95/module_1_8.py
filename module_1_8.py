889646a279b023a1bd4109e1e1a5b26bff801ff1
buninegor95 <buninegor95@gmail.com>
2024-10-20 18:32:38 +0300

    correctly made module_1_8.py
    я выставил не ту версию файла а теперь эта нужная версия

--- module_1_8.py
+++ module_1_8.py
@@ -1,6 +1,20 @@
 my_dict = {'bobre': 666666666, 'neger': 888888888}
-my_dict['belka'] = 9186545786
+print('my_dict:', my_dict)
+print('Existing value:', my_dict.get('neger'))
+print('Deleted value:', my_dict.get('bomge'))
 my_dict.update({"strelka" : 6573876734})
-print(my_dict)
-print(my_dict.get('neger'))
-print(my_dict.get('bomge'))
+my_dict['belka'] = 9186545786
+a = my_dict.pop('neger')
+print('удалён',a)
+print('Modified dictionary:', my_dict)
+
+
+my_set = {6, 9, 3, 7, 6, 7, 3, 9, 42.314 ,'петух','блатной','петух', }
+my_set.add(5)
+my_set.add(10)
+my_set.discard('петух')
+print(set(my_set))
+
+
+
+
