dc2d2f41916637a032d0419fe78dc0bbc03e53ab
buninegor95 <buninegor95@gmail.com>
2024-10-20 18:28:59 +0300

    Add files via upload

---
+++ module_1_8.py
@@ -0,0 +1,6 @@
+my_dict = {'bobre': 666666666, 'neger': 888888888}
+my_dict['belka'] = 9186545786
+my_dict.update({"strelka" : 6573876734})
+print(my_dict)
+print(my_dict.get('neger'))
+print(my_dict.get('bomge'))
